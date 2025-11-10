07f321eb17a9fe6477a79bbb46bce1baa79aab10
hajime <haojime1109@gmail.com>
2025-11-10 16:10:41 +0900

    スマホからのアクセス許可
    Signed-off-by: hajime <haojime1109@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -17,6 +17,6 @@ export default defineConfig({
   },
 
   server: {
-    host: '0.0.0.0' // すべてのIPアドレスからの接続を許可
+    host: "0.0.0.0",
   }
 })
